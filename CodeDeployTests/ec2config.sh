e597df52e531c59e9389fa438b1a717b13eb9467
miguicap <miguel-angel.orozco-tortosa.external@capgemini.com>
2023-03-29 19:44:17 +0200

    Commit

---
+++ CodeDeployTests/ec2config.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+yum update -y
+yum install -y ruby
+
+# Install Docker
+amazon-linux-extras install docker -y
+service docker start
+systemctl enable docker
+usermod -a -G docker ec2-user
+chmod 666 /var/run/docker.sock
+
+# Install Docker Compose
+curl -L https://github.com/docker/compose/rel... -s)-$(uname -m) -o /usr/local/bin/docker-compose
+chmod +x /usr/local/bin/docker-compose
+
+# Install code deploy agent
+wget https://aws-codedeploy-us-east-1.s3.u...
+chmod +x ./install
+./install auto
